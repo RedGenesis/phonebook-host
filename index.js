a4da9465b45af58ebd4a1d09f33fafc1045fa3f5
unknown <willyyumbum@yahoo.com>
2024-12-09 05:34:06 -0500

    added dist

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 
@@ -9,7 +10,8 @@ const app = express();
 //     console.log('---');
 //     next();
 // };
-
+app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 // app.use(requestLogger);
 
@@ -138,7 +140,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3004;
+// const PORT = 3004;
+const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
